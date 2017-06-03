85df10d5f1689d2d18e0121720f5f8c6f40f422e
amodmahajan <amod.mahajan@tatahealth.com>
2017-06-03 12:38:10 +0530

    First Commit

---
+++ UdemyLearning/src/Package1/TC_003.java
@@ -0,0 +1,21 @@
+package Package1;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+
+
+public class TC_003 {
+
+	@Test
+	public void demoTestCase003()
+	{
+		System.setProperty("webdriver.chrome.driver","D:\\Organised\\Selenium_Frameworks\\FrameworkByMukesh\\exefiles\\chromedriver.exe");
+		WebDriver driver= new ChromeDriver();
+		driver.get("https://www.google.com");
+		System.out.println(driver.getTitle());
+		System.out.println(driver.getCurrentUrl());
+		driver.quit(); 
+	}
+}
